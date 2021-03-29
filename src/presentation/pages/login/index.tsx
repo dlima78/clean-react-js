df42d4e7f0d35ed9c82c9ac0f0ea18174b9813f1
Eduardo Lima <dlima78@hotmail.com>
2021-03-29 14:42:48 -0300

    test: ensure Login starts with inputs required state

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -8,25 +8,25 @@ import {
 import Context from '@/presentation/contexts/form/form-context'
 import * as S from './styled'
 
-type StateProps = {
-  isLoading: boolean
-  errorMesssage: string
-}
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMesssage: ''
+  const [state] = useState({
+    isLoading: false
+  })
+
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
   return (
     <S.LoginWrapper>
       <Header />
-      <Context.Provider value = {state}>
+      <Context.Provider value = {{ state, errorState }}>
       <S.Form>
         <S.TitleLogin>Login</S.TitleLogin>
-        <Input type="text" placeholder="Digite seu email" />
-        <Input type="password" placeholder="Digite sua senha" />
-        <S.Button role="submit" disabled>Entrar</S.Button>
+        <Input type="text" name='email' placeholder="Digite seu email" />
+        <Input type="password" name='password' placeholder="Digite sua senha" />
+        <S.Button disabled>Entrar</S.Button>
         <S.Link>Criar Conta</S.Link>
         <FormStatus />
       </S.Form>
