47be8219f9d6160314ba5e3789328c0e32907c2c
Eduardo Lima <dlima78@hotmail.com>
2021-03-29 17:29:51 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import { render, screen, RenderResult } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Validation } from '@/presentation/protocols'
 import Login from './'
 
-const makeSut = (): RenderResult => render(<Login />)
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
+}
+
+type SutTypes = {
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  render(<Login validation={validationSpy} />)
+  return {
+    validationSpy
+  }
+}
 
 describe('Login Component', () => {
   test('Should start with initial state', () => {
@@ -18,5 +39,14 @@ describe('Login Component', () => {
     const passwordStatus = screen.getByRole('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  test('Should call Validation with correct values', () => {
+    const { validationSpy } = makeSut()
+    const emailInput = screen.getByRole('email')
+    userEvent.type(emailInput, 'any_email')
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
