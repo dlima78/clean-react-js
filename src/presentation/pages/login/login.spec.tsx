e8437e95f1ab577771d0a485fd49345d4adce266
Eduardo Lima <dlima78@hotmail.com>
2021-03-30 10:39:58 -0300

    refactor: change validationStub to initialize without error

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,61 +1,60 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, RenderResult, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import faker from 'faker'
 
 import { ValidationStub } from '@/presentation/tests'
 import Login from './'
 
-type SutTypes = {
-  validationStub: ValidationStub
+type SutParams = {
+  validationError: string
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (params?: SutParams): RenderResult => {
   const validationStub = new ValidationStub()
-  validationStub.errorMessage = faker.random.words()
-  render(<Login validation={validationStub} />)
-  return {
-    validationStub
-  }
+  validationStub.errorMessage = params?.validationError
+  return render(<Login validation={validationStub} />)
 }
 
 describe('Login Component', () => {
   test('Should start with initial state', () => {
-    const { validationStub } = makeSut()
+    const validationError = faker.random.words()
+    makeSut({ validationError })
     const errorWrap = screen.getByRole('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = screen.getByRole('button',
       { name: /entrar/i }) as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
     const emailStatus = screen.getByRole('email-status')
-    expect(emailStatus.title).toBe(validationStub.errorMessage)
+    expect(emailStatus.title).toBe(validationError)
     expect(emailStatus.textContent).toBe('🔴')
     const passwordStatus = screen.getByRole('password-status')
-    expect(passwordStatus.title).toBe(validationStub.errorMessage)
+    expect(passwordStatus.title).toBe(validationError)
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
   test('Should show email error if Validation fails', () => {
-    const { validationStub } = makeSut()
+    const validationError = faker.random.words()
+    makeSut({ validationError })
     const emailInput = screen.getByRole('email')
     userEvent.type(emailInput, faker.internet.email())
     const emailStatus = screen.getByRole('email-status')
-    expect(emailStatus.title).toBe(validationStub.errorMessage)
+    expect(emailStatus.title).toBe(validationError)
     expect(emailStatus.textContent).toBe('🔴')
   })
 
   test('Should show password error if Validation fails', () => {
-    const { validationStub } = makeSut()
+    const validationError = faker.random.words()
+    makeSut({ validationError })
     const passwordInput = screen.getByRole('password')
     userEvent.type(passwordInput, faker.internet.password())
     const passwordStatus = screen.getByRole('password-status')
-    expect(passwordStatus.title).toBe(validationStub.errorMessage)
+    expect(passwordStatus.title).toBe(validationError)
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
   test('Should show valid email state if Validation succeeds', () => {
-    const { validationStub } = makeSut()
-    validationStub.errorMessage = null
+    makeSut()
     const emailInput = screen.getByRole('email')
     userEvent.type(emailInput, faker.internet.email())
     const emailStatus = screen.getByRole('email-status')
@@ -64,8 +63,7 @@ describe('Login Component', () => {
   })
 
   test('Should show valid password state if Validation succeeds', () => {
-    const { validationStub } = makeSut()
-    validationStub.errorMessage = null
+    makeSut()
     const passwordInput = screen.getByRole('password')
     userEvent.type(passwordInput, faker.internet.email())
     const passwordStatus = screen.getByRole('password-status')
@@ -74,8 +72,7 @@ describe('Login Component', () => {
   })
 
   test('Should enable submit button if form is valid', () => {
-    const { validationStub } = makeSut()
-    validationStub.errorMessage = null
+    makeSut()
     const emailInput = screen.getByRole('email')
     userEvent.type(emailInput, faker.internet.email())
     const passwordInput = screen.getByRole('password')
