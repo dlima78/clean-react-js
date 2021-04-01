9b03a3a6e7b9a2ac296dab4773f684f46637503f
Eduardo Lima <dlima78@hotmail.com>
2021-04-01 16:37:41 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,32 @@
+import { ValidationComposite } from './validation-composite'
+import { FieldValidationSpy } from '@/validation/tests/mock-validation'
+
+import faker from 'faker'
+
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationSpy
+}
+
+const makeSut = (fieldName: string): SutTypes => {
+  const fieldValidationSpy = [
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
+  ]
+  const sut = new ValidationComposite(fieldValidationSpy)
+  return {
+    sut,
+    fieldValidationSpy
+  }
+}
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationSpy } = makeSut(fieldName)
+    const error1 = faker.random.word()
+    fieldValidationSpy[0].error = new Error(error1)
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(error1)
+  })
+})
