184b7e146cebe6a658ac9d73dc1a0d8247ef0fce
Eduardo Lima <dlima78@hotmail.com>
2021-04-01 11:56:13 -0300

    refactor: change SUT creation factory

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -2,26 +2,17 @@ import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
 import faker from 'faker'
 
-type SutTypes = {
-  sut: RequiredFieldValidation
-}
-
-const makeSut = (field: string): SutTypes => {
-  const sut = new RequiredFieldValidation(field)
-  return {
-    sut
-  }
-}
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.random.word())
 
 describe('RequiredFieldValidation', () => {
   test('Should return erro if field is empty', () => {
-    const { sut } = makeSut('email')
+    const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test('Should return falsy if field is not empty', () => {
-    const { sut } = makeSut('email')
+    const sut = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toBeFalsy()
   })
