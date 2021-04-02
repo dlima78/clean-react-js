64ffa7aa6b7c8779fd53c345fb35cae9f834198a
Eduardo Lima <dlima78@hotmail.com>
2021-04-02 17:20:55 -0300

    test: ensure ValidationComposite use correct validations

--- src/main/factories/pages/login/login-validation-factory.ts
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -4,6 +4,6 @@ import { ValidationBuilder } from '@/validation/validators/builder/validation-bu
 export const makeLoginValidation = (): ValidationComposite => {
   return ValidationComposite.build([
     ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
+    ...ValidationBuilder.field('password').required().min(3).build()
   ])
 }
