9bf62cbf64f1b27776ef2c0188cd40993c301be5
Eduardo Lima <dlima78@hotmail.com>
2021-04-03 19:03:27 -0300

    refactor: change faker types

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -22,7 +22,7 @@ describe('ValidationBuilder', () => {
 
   test('Should return MinLengthValidation', () => {
     const field = faker.database.column()
-    const length = faker.random.number()
+    const length = faker.datatype.number()
     const validations = ValidationBuilder.field(field).min(length).build()
     expect(validations).toStrictEqual([new MinLengthValidation(field, length)])
   })
