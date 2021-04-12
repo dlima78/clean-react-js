f35f90ec698ac88bedf74a28ce351dec9d017354
Eduardo Lima <dlima78@hotmail.com>
2021-04-12 17:37:27 -0300

    test: ensure LocalSaveAccessToken throws if accessToken is undefined

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -2,6 +2,7 @@ import { LocalSaveAccessToken } from './local-save-access-token'
 import { SetStorageMock } from '@/data/tests'
 
 import faker from 'faker'
+import { UnexpectedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: LocalSaveAccessToken
@@ -32,5 +33,11 @@ describe('LocalSaveAccessToken', () => {
       .mockRejectedValueOnce(new Error())
     const promise = sut.save(faker.datatype.uuid())
     await expect(promise).rejects.toThrow(new Error())
+  })
+
+  test('Should throw if accessToken is falsy', async () => {
+    const { sut } = makeSut()
+    const promise = sut.save(undefined)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
