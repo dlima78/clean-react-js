3c6ad50da6b2f7d16a4f4e66e849365b100d2db6
Eduardo Lima <dlima78@hotmail.com>
2021-04-12 14:43:07 -0300

    test: ensure Login presents valid state if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -11,8 +11,8 @@ describe('Login', () => {
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴')
-    cy.getByTestId('error-wrap').should('not.have.descendants')
     cy.get('button[type=submit]').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
   it('Should present error state if form is invalid', () => {
@@ -24,7 +24,20 @@ describe('Login', () => {
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo inválido')
       .should('contain.text', '🔴')
-    cy.getByTestId('error-wrap').should('not.have.descendants')
     cy.get('button[type=submit]').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo')
+      .should('contain.text', '🟢')
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo')
+      .should('contain.text', '🟢')
+    cy.get('button[type=submit]').should('not.have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
