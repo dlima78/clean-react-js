73922c060ed7ef3dfe27c868b4ac2b0595bd1245
Eduardo Lima <dlima78@hotmail.com>
2021-04-20 15:38:04 -0300

    test: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -24,4 +24,18 @@ describe('Name of the group', () => {
     expect(screen.getByRole('month')).toHaveTextContent('jan')
     expect(screen.getByRole('year')).toHaveTextContent('2021')
   })
+
+  test('Should render with correct values ', () => {
+    const survey = {
+      ...mockSurveyModel(),
+      date: new Date('2019-06-03T06:00:00Z'),
+      didAnswer: false
+    }
+    makeSut(survey)
+    expect(screen.getByRole('icon')).toHaveProperty('src', IconName.thumbDown)
+    expect(screen.getByRole('question')).toHaveTextContent(survey.question)
+    expect(screen.getByRole('day')).toHaveTextContent('03')
+    expect(screen.getByRole('month')).toHaveTextContent('jun')
+    expect(screen.getByRole('year')).toHaveTextContent('2019')
+  })
 })
