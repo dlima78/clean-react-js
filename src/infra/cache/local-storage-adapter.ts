5a73df95d7bbeadbe9511a28e764429d541349fc
Eduardo Lima <dlima78@hotmail.com>
2021-04-23 13:26:13 -0300

    fix: fix bug

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -4,8 +4,9 @@ export class LocalStorageAdapter implements SetStorage, GetStorage {
   set (key: string, value: object): void {
     if (value) {
       localStorage.setItem(key, JSON.stringify(value))
+    } else {
+      localStorage.removeItem(key)
     }
-    localStorage.removeItem(key)
   }
 
   get (key: string): any {
