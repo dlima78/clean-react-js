eb517fc348ef56d92989461045f1e54432993d13
Eduardo Lima <dlima78@hotmail.com>
2021-04-26 10:47:57 -0300

    test: ensure SurveyList present correct survey items

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,42 @@
+import faker from 'faker'
+
+const baseUrl = Cypress.config().baseUrl
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    cy.fixture('account').then(account => {
+      localStorage.setItem('account', JSON.stringify(account))
+    })
+  })
+  it('Should logout on AcessDeniedError', () => {
+    cy.intercept('GET', '/surveys', {
+      statusCode: 403,
+      response: {
+        error: faker.random.words()
+      }
+    }).as('request')
+    cy.visit('')
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
+
+  it('Should present correct username', () => {
+    cy.intercept('GET', '/surveys', {
+      statusCode: 500,
+      response: {
+        error: faker.random.words()
+      }
+    }).as('request')
+    cy.visit('')
+    const { name } = JSON.parse(localStorage.getItem('account'))
+    cy.getByTestId('username').should('contain.text', name)
+  })
+
+  it('Should present correct survey-items', () => {
+    cy.intercept('GET', '/surveys', {
+      statusCode: 200,
+      response: 'fx:survey-list'
+    }).as('request')
+    cy.visit('')
+    cy.get('li:empty').should('have.length', 4)
+  })
+})
