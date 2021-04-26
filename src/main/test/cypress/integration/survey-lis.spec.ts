6cb8374f8e35e2bc4213da8a25b5f590b5b65cb5
Eduardo Lima <dlima78@hotmail.com>
2021-04-26 10:10:34 -0300

    refactor: organize integration test with fixtures

--- src/main/test/cypress/integration/survey-lis.spec.ts
+++ src/main/test/cypress/integration/survey-lis.spec.ts
@@ -4,9 +4,9 @@ const baseUrl = Cypress.config().baseUrl
 
 describe('SurveyList', () => {
   beforeEach(() => {
-    localStorage.setItem('account', JSON.stringify({
-      accessToken: faker.datatype.uuid(), name: faker.name.findName()
-    }))
+    cy.fixture('account').then(account => {
+      localStorage.setItem('account', JSON.stringify(account))
+    })
   })
   it('Should logout on AcessDeniedError', () => {
     cy.intercept('GET', '/surveys', {
@@ -17,16 +17,5 @@ describe('SurveyList', () => {
     }).as('request')
     cy.visit('')
     cy.url().should('eq', `${baseUrl}/login`)
-  })
-  it('Should present correct username', () => {
-    cy.intercept('GET', '/surveys', {
-      statusCode: 500,
-      response: {
-        error: faker.random.words()
-      }
-    }).as('request')
-    cy.visit('')
-    const { name } = JSON.parse(localStorage.getItem('account'))
-    cy.getByTestId('username').should('contain.text', name)
   })
 })
