0a9af644d3a76919b95950de31392842edec185f
Eduardo Lima <dlima78@hotmail.com>
2021-05-04 10:55:49 -0300

    test: ensure SurveyResult presents correct result on success

--- src/domain/tests/mock-survey-result.ts
+++ src/domain/tests/mock-survey-result.ts
@@ -9,12 +9,12 @@ export const mockSurveyResultModel = (): LoadSurveyResult.Model => ({
     answer: faker.random.word(),
     count: faker.datatype.number(),
     percent: faker.datatype.number(100),
-    isCurrentAccountanswer: faker.datatype.boolean()
+    isCurrentAccountanswer: true
   }, {
     answer: faker.random.word(),
     count: faker.datatype.number(),
     percent: faker.datatype.number(100),
-    isCurrentAccountanswer: faker.datatype.boolean()
+    isCurrentAccountanswer: false
   }],
   date: faker.date.recent()
 })
--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 
-import { mockAccountModel, LoadSurveyResultSpy } from '@/domain/tests'
+import { mockAccountModel, LoadSurveyResultSpy, mockSurveyResultModel } from '@/domain/tests'
 import { SurveyResult } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
 
@@ -9,8 +9,9 @@ type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (surveyResult = mockSurveyResultModel()): SutTypes => {
   const loadSurveyResultSpy = new LoadSurveyResultSpy()
+  loadSurveyResultSpy.surveyResult = surveyResult
   render(
     <ApiContext.Provider value={ {
       setCurrentAccount: jest.fn(),
@@ -38,5 +39,29 @@ describe('SurveyResult Component', () => {
     const { loadSurveyResultSpy } = makeSut()
     await waitFor(() => screen.getByRole('survey-result'))
     expect(loadSurveyResultSpy.callscount).toBe(1)
+  })
+
+  test('Should present SurveyResult data on success', async () => {
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2021-01-10T00:00:00')
+    })
+
+    makeSut(surveyResult)
+    await waitFor(() => screen.getByRole('survey-result'))
+    expect(screen.getByRole('day')).toHaveTextContent('10')
+    expect(screen.getByRole('month')).toHaveTextContent('jan')
+    expect(screen.getByRole('year')).toHaveTextContent('2021')
+    expect(screen.getByRole('question')).toHaveTextContent(surveyResult.question)
+    expect(screen.getByTestId('answers').childElementCount).toBe(2)
+    const images = screen.getAllByRole('image')
+    expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+    expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+    expect(images[1]).toBeFalsy()
+    const answers = screen.getAllByRole('answer')
+    expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+    expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+    const percents = screen.getAllByRole('percent')
+    expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
+    expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
   })
 })
