551b040732de437edc4777f54685a9ac1dbaba4a
Eduardo Lima <dlima78@hotmail.com>
2021-05-05 14:17:06 -0300

    feat: ensure RemoteSaveSurveyResult returns SurveyResult on success

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -15,8 +15,11 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
       method: 'put',
       body: params
     })
+    const remoteSurveyResult = httpResponse.body
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return
+      case HttpStatusCode.ok: return Object.assign(
+        {}, remoteSurveyResult,
+        { date: new Date(remoteSurveyResult.date) })
       case HttpStatusCode.forbidden: throw new AccessDeniedError()
       default: throw new UnexpectedError()
     }
