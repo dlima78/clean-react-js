19fe53fdaf1e04c773108151ab3f11346e1304b4
Eduardo Lima <dlima78@hotmail.com>
2021-05-10 13:58:07 -0300

    refactor: remove unnecessary dependency

--- src/presentation/pages/survey-result/components/result/index.tsx
+++ src/presentation/pages/survey-result/components/result/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import FlipMove from 'react-flip-move'
 import { useHistory } from 'react-router-dom'
 
 import { LoadSurveyResult } from '@/domain/usecases'
@@ -19,12 +18,8 @@ const Result: React.FC<Props> = ({ surveyResult }: Props) => {
         <Calendar date={surveyResult.date} />
         <S.Question role='question'>{surveyResult.question} </S.Question>
       </S.HGroup>
-      <S.ResultList >
-        <FlipMove data-testid='answers'>
-          <>
-            {surveyResult.answers.map(answer => <Answer key={answer.answer} answer={answer} />)}
-          </>
-        </FlipMove>
+      <S.ResultList data-testid='answers'>
+        {surveyResult.answers.map(answer => <Answer key={answer.answer} answer={answer} />)}
       </S.ResultList>
       <S.Button role='back-button' onClick={goBack} >Voltar</S.Button>
     </>
